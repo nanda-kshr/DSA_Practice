d09bba61a1888e1394dfc423e0984eaf17ab37b2
Nandakshore <sitedeals999@gmail.com>
2025-08-12 03:53:35 +0530

    Arrays

---
+++ DataStructure/Arrays.java
@@ -0,0 +1,8 @@
+package DataStructure;
+
+public class Arrays{
+    public static void main(String[] args){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Length is "+ array.length);
+    }
+}
